c1a7477f08b46cc4a4ba0fcd7660c872c7620ebe
조태준 <taejun1999@gmail.com>
2021-12-20 18:45:56 +0900

    no message

---
+++ columnsData.js
@@ -0,0 +1,6 @@
+[
+	{caption: 'col1-input', ref: 'col1', width: '200px', type: 'input'},
+	{caption: 'col2-output', ref: 'col2', width: '200px', type: 'output'},
+	{caption: 'col3-datepicker', ref: 'col3', width: '200px', type: 'datepicker'},
+	{caption: 'col4-textarea', ref: 'col4', width: '200px', type: 'textarea'},
+]
